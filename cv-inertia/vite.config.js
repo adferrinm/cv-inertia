a3cc2e89e8c4b33b050c8cbc194daa3474e6cf5e
adferrinm <62492197+adferrinm@users.noreply.github.com>
2025-11-26 12:33:19 -0500

    Simplify Vite config and improve build debugging

--- cv-inertia/vite.config.js
+++ cv-inertia/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -17,13 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-            },
-        },
-    },
 });
